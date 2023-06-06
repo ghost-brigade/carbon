7b59723f0521f5bc94c7bd7579bdd2fb5dd4e047
Anthony ARJONA <anthony.arjona@outlook.com>
2023-06-06 16:06:52 +0200

    feat: prisma sql migration

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,11 @@
-import { Logger, VERSION_NEUTRAL, VersioningType } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { Logger, VERSION_NEUTRAL, VersioningType } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app/app.module";
 import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
 
   app.use(helmet());
   app.enableCors({ origin: [process.env?.CORS_ORIGIN || "*"] });
@@ -14,7 +15,7 @@ async function bootstrap() {
     header: "Accept-Version",
   });
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(port);
 
   Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
