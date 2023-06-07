8da7c4cdab4f5383315818aa50adfc6d8006bd8f
MoulinLouis <moulin.louis93@gmail.com>
2023-06-07 09:08:29 +0000

    chore: skill guards + makefile update migration

--- apps/backend/src/skill/skill.controller.ts
+++ apps/backend/src/skill/skill.controller.ts
@@ -6,14 +6,18 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from "@nestjs/common";
 import { SkillService } from "./skill.service";
-import { SkillCreateType, SkillUpdateType } from "@carbon/zod";
+import { SkillCreateSchema, SkillCreateType, SkillUpdateSchema, SkillUpdateType } from "@carbon/zod";
+import { Public } from "../core/decorators/public.decorator";
+import { ZodGuard } from "../core/guard/zod/zod.guard";
 
 @Controller("skill")
 export class SkillController {
   constructor(private readonly skillService: SkillService) {}
 
+  @UseGuards(new ZodGuard("body", SkillCreateSchema))
   @Post()
   async create(@Body() createSkill: SkillCreateType) {
     return this.skillService.create(createSkill);
@@ -29,6 +33,7 @@ export class SkillController {
     return await this.skillService.findOne(id);
   }
 
+  @UseGuards(new ZodGuard("body", SkillUpdateSchema))
   @Patch(":id")
   async update(@Param("id") id: string, @Body() updateSkill: SkillUpdateType) {
     return await this.skillService.update(id, updateSkill);
