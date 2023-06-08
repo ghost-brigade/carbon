fc649a9562d945207527e30eef560f7cacbcc0c6
Alexis LOURS <alexislours@protonmail.com>
2023-06-08 11:24:08 +0200

    feat: show badges dynamically

--- apps/frontend/src/app/views/profile/profile.component.ts
+++ apps/frontend/src/app/views/profile/profile.component.ts
@@ -41,4 +41,33 @@ export class ProfileComponent implements OnInit {
         },
       });
   }
+
+  getYearsOfExperience(): string {
+    if (!this.profile) return "0";
+    const startDate = this.profile.entryDate;
+
+    const today = new Date();
+    const start = new Date(startDate);
+    const years = today.getFullYear() - start.getFullYear();
+    const months = today.getMonth() - start.getMonth();
+    const days = today.getDate() - start.getDate();
+
+    return (years + months / 12 + days / 365).toFixed(1);
+  }
+
+  uniqueBadges() {
+    if (!this.profile) return [];
+    // @ts-ignore
+    // remove duplicate object based on 'achievement' property
+    return this.profile.UserAchievement.reduce((acc, current) => {
+      const x = acc.find(
+        (item: any) => item.achievement === current.achievement
+      );
+      if (!x) {
+        return acc.concat([current]);
+      } else {
+        return acc;
+      }
+    }, []).slice(0, 3);
+  }
 }
