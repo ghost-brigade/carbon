e44c9654fde0d2d65ab9641831a70b08c90fb1cf
3kezoh <3kezoh@gmail.com>
2023-06-10 01:57:15 +0200

    feat: add dummy image in user list

--- apps/frontend/src/app/core/components/users/users.component.ts
+++ apps/frontend/src/app/core/components/users/users.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { User } from "../../../shared/types/users";
-import { Observable } from "rxjs";
 
 @Component({
   selector: "carbon-users",
