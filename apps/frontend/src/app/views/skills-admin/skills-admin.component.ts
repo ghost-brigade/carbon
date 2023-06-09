f7de76e3c61b2232cff91dcee0cc59fe8a426bd8
Alexis LOURS <alexislours@protonmail.com>
2023-06-09 12:57:36 +0200

    feat: add admin components

---
+++ apps/frontend/src/app/views/skills-admin/skills-admin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  selector: "carbon-skills-admin",
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: "./skills-admin.component.html",
+  styleUrls: ["./skills-admin.component.css"],
+})
+export class SkillsAdminComponent {}
