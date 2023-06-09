a03554463f533d141f12e1f0f1e461180095f05d
3kezoh <3kezoh@gmail.com>
2023-06-10 01:54:07 +0200

    fix: fix file module dependencies

--- apps/backend/src/file/file.module.ts
+++ apps/backend/src/file/file.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { FileService } from "./file.service";
 import { FileController } from "./file.controller";
 import { PrismaService } from "../prisma.service";
+import { UserService } from "../user/user.service";
 
 @Module({
   controllers: [FileController],
-  providers: [PrismaService, FileService],
+  providers: [PrismaService, FileService, UserService],
   exports: [FileService],
 })
 export class FileModule {}
