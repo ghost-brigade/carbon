3b3580a4677bcdd1ca5500c8264cd0ba0d619d25
Anthony ARJONA <anthony.arjona@outlook.com>
2023-06-11 13:20:27 +0200

    feat: remove adr from response with an interceptor

---
+++ apps/backend/src/core/interceptors/average-daily-rating.interceptor.ts
@@ -0,0 +1,44 @@
+import { RolesValues } from "@carbon/enum";
+import { UserType } from "@carbon/zod";
+import {
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  Injectable,
+} from "@nestjs/common";
+import { map } from "rxjs/operators";
+
+@Injectable()
+export class AverageDailyRatingInterceptor implements NestInterceptor {
+  intercept(_context: ExecutionContext, next: CallHandler) {
+    const request = _context.switchToHttp().getRequest();
+
+    if (
+      request._user.role === RolesValues.HR ||
+      request._user.role === RolesValues.COMMERCIAL
+    ) {
+      return next.handle();
+    }
+
+    return next.handle().pipe(map((value) => this.removeADR(value)));
+  }
+
+  private removeADR(object) {
+    if (Array.isArray(object)) {
+      return object.map((obj) => this.removeADR(obj));
+    }
+
+    if (typeof object === "object" && object !== null) {
+      console.log(object);
+      for (const key in object) {
+        if (key === "averageDailyRate") {
+          delete object[key];
+        } else {
+          object[key] = this.removeADR(object[key]);
+        }
+      }
+    }
+
+    return object;
+  }
+}
